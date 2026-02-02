029f3c9ef7c41a8e93c2f229497801f823b14f28
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2026-02-02 16:38:08 -0600

    Update Github actions and add service

--- services/facebook_service.py
+++ services/facebook_service.py
@@ -1,6 +1,22 @@
 import os
 from config.facebook import (FACEBOOK_API_VERSION, FACEBOOK_PAGE_ACCESS_TOKEN, FACEBOOK_PAGE_ID)
 
+GRAPH_BASE_URL = f"https://graph.facebook.com/{FACEBOOK_API_VERSION}"
+
+def pubish_to_facebook(message: str, image_url: str | None = None):
+    if not FACEBOOK_PAGE_ACCESS_TOKEN:
+        raise ValueError("FACEBOOK_PAGE_ACCESS_TOKEN is not configured")
+
+    # 📸 Post con image
+
+    if image_url:
+        url = f"{GRAPH_BASE_URL}/{FACEBOOK_PAGE_ID}/photos"
+        payload = {
+            "url"
+        }
+
+
+
 def publish_to_facebook_mock(text, image_url):
 
     """
