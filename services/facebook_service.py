7dd4c6b68683aec03dae60bee369a626e16d65f8
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2026-02-02 16:55:32 -0600

    Add publish to facebook method

--- services/facebook_service.py
+++ services/facebook_service.py
@@ -1,4 +1,4 @@
-import os
+import requests
 from config.facebook import (FACEBOOK_API_VERSION, FACEBOOK_PAGE_ACCESS_TOKEN, FACEBOOK_PAGE_ID)
 
 GRAPH_BASE_URL = f"https://graph.facebook.com/{FACEBOOK_API_VERSION}"
@@ -7,15 +7,31 @@ def pubish_to_facebook(message: str, image_url: str | None = None):
     if not FACEBOOK_PAGE_ACCESS_TOKEN:
         raise ValueError("FACEBOOK_PAGE_ACCESS_TOKEN is not configured")
 
-    # 📸 Post con image
-
     if image_url:
+        # Post con image + texto
         url = f"{GRAPH_BASE_URL}/{FACEBOOK_PAGE_ID}/photos"
         payload = {
-            "url"
+            "url": image_url,
+            "caption": message,
+            "access_token": FACEBOOK_PAGE_ACCESS_TOKEN
+        }
+    else:
+        # Post solo texto
+        url=f"{GRAPH_BASE_URL}/{FACEBOOK_PAGE_ID}/feed"
+        payload = {
+            "message": message,
+            "access_token": FACEBOOK_PAGE_ACCESS_TOKEN
         }
 
+    response = requests.post(url, data=payload)
 
+    if response.status_code != 200:
+        raise Exception(f"Facebook API error: {response.text}")
+
+    data = response.json()
+
+    # Facebook retorna diferentes claves segun el endpoint
+    return data.get("post_id") or data.get("id")
 
 def publish_to_facebook_mock(text, image_url):
 
